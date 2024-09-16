d24af408d273c519c81fb4eb80f13ea36cc3913b
mp97dev <michele.pasetto@eaziu.com>
2024-09-16 21:10:18 +0200

    close on finalize

--- client/src/app/components/login-dialog/login-dialog.component.ts
+++ client/src/app/components/login-dialog/login-dialog.component.ts
@@ -5,13 +5,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { AuthService } from '../../services/auth.service';
-import { catchError, of, tap } from 'rxjs';
+import { catchError, of, take, tap } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-login-dialog',
   standalone: true,
-  imports: [ ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatDialogModule,  MatButtonModule ],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatDialogModule, MatButtonModule],
   templateUrl: './login-dialog.component.html',
   styleUrl: './login-dialog.component.scss'
 })
@@ -20,7 +20,7 @@ export class LoginDialogComponent {
   public readonly action
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) data: {action: 'login' | 'signup'},
+    @Inject(MAT_DIALOG_DATA) data: { action: 'login' | 'signup' },
     private dialogRef: MatDialogRef<LoginDialogComponent>,
     private auth: AuthService
   ) {
@@ -33,20 +33,23 @@ export class LoginDialogComponent {
   })
 
   onSubmit() {
-    if(!this.form.valid) throw new Error('Invalid login form')
+    if (!this.form.valid) throw new Error('Invalid login form')
     const value = this.form.value
 
-  const sub = this.action === 'login' ? this.auth.login(value.username ?? '', value.password ?? '') : this.auth.register(value.username ?? '', value.password ?? '')
+    const sub = this.action === 'login' ? this.auth.login(value.username ?? '', value.password ?? '') : this.auth.register(value.username ?? '', value.password ?? '')
 
-  sub.pipe(
-      tap(() => this.dialogRef.close()),
-      catchError(err => {console.error(err); return of(null)})
-    ).subscribe()
+    return sub.pipe(
+      take(1),
+      catchError(err => { console.error(err); return of(null) })
+    ).subscribe(
+      {
+        complete: () => this.dialogRef.close(),
+      }
+    )
   }
 
   goToOther() {
     this.dialogRef.close(this.action === 'login' ? 'signup' : 'login')
   }
-
 
 }
